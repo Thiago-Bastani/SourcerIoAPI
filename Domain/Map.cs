471321e8c7d6ee9a080b1f284462fe0b98eaf1d3
Thiago Bastani <thiagobastani.tb@gmail.com>
2023-07-24 15:05:53 -0300

    Guid for map

--- Domain/Map.cs
+++ Domain/Map.cs
@@ -2,8 +2,10 @@ using SocketIOSharp.Server;
 namespace SorcerIo.Domain;
 public class Map
 {
+    public Guid Id { get; set; }
     public string Layout { get; set; }
     public Player Owner { get; set; }
+    public IList<Player>? ConnectedPlayers { get; set; }
     public SocketIOServer Socket { get; set; }
     public Map(string layout, Player owner, SocketIOServer socket)
     {
