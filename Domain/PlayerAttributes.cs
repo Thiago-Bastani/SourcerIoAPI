243df2ca471aeef5c5f7c6c5385a17dadca88a63
Thiago Bastani <thiagobastani.tb@gmail.com>
2023-07-24 11:10:42 -0300

    Segregate PlaeyerAttr. into diff. table

--- Domain/PlayerAttributes.cs
+++ Domain/PlayerAttributes.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SorcerIo.Domain;
 public class PlayerAttributes
 {
+    [Key]
+    public int Id { get; set; }
+
+    [ForeignKey("Player")]
+    public int PlayerId { get; set; }
+    public Player? Player { get; set; }
     public int Speed { get; set; }
     public int Strength { get; set; }
 
-    public PlayerAttributes(int speed, int strength)
-    {
-        Speed = speed;
-        Strength = strength;
-    }
 }
