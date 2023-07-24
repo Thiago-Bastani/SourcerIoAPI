8f9b20c4fb819bfda63074f97e0018c47d4067f4
Thiago Bastani <thiagobastani.tb@gmail.com>
2023-07-24 11:10:11 -0300

    Player Domain

--- Domain/Player.cs
+++ Domain/Player.cs
@@ -1,14 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SorcerIo.Domain;
+
 public class Player
 {
-    public string Name { get; set; }
-    public string Format { get; set; }
-    public string Color { get; set; }
-    public PlayerAttributes Attributes { get; set; }
-    public Player(string name, string format, string color)
-    {
-        Name = name;
-        Format = format;
-        Color = color;
-        Attributes = new PlayerAttributes(100, 100);
-    }
+    [Key]
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Format { get; set; }
+    public string? Color { get; set; }
+    public PlayerAttributes? Attributes { get; set; }
+
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+    public User? User { get; set; }
+
 }
