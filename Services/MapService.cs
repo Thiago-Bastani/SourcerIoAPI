909b0ccbcbb2db0a78c6dd4cdf440edc9b23c4f4
Thiago Bastani <thiagobastani.tb@gmail.com>
2023-07-24 15:53:39 -0300

    adding socket handling functions

--- Services/MapService.cs
+++ Services/MapService.cs
@@ -8,16 +8,17 @@ namespace SorcerIo.Services
     public class MapService
     {
 
-        private static List<Map> _currentMaps = new List<Map>();
+        private static List<Map> _runningMaps = new List<Map>();
+        private static List<SocketIOServer> _runningSockets = new List<SocketIOServer>();
+        private const ushort START_PORT = 3000;
 
-        public void CreateMap(Player owner, ushort port, string layout = "standard")
+        public void CreateMap(Player owner, string layout = "standard")
         {
-            SocketIOServer socket = new SocketIOServer(new SocketIOServerOption(port));
-            Map map = new Map(layout, owner, socket);
-            if (FindMap(owner, port) == null)
+            Map map = new Map(layout, owner);
+            if (FindMap(owner) == null)
             {
-                _currentMaps.Add(map);
-                StartServer(map);
+                _runningMaps.Add(map);
+                connectMapToASocket(map);
             }
             else
             {
@@ -25,10 +26,26 @@ namespace SorcerIo.Services
             }
         }
 
-        public void StartServer(Map map)
+        public void connectMapToASocket(Map map)
         {
-            SetServerRules(map);
-            map.Socket.Start();
+            SocketIOServer socket = new SocketIOServer(new SocketIOServerOption(HandlePortGeneration()));
+            // add socket if it doesn't already exist on the list
+            if (_runningSockets.Find(rsocket => rsocket.Option.Port == socket.Option.Port) == null)
+            {
+                _runningSockets.Add(socket);
+                SetServerRules(map);
+                StartServer(socket);
+            }
+            else
+            {
+                // if the port is beeing used by another socket, map gets that socket to run on it
+                map.Socket = _runningSockets[_runningSockets.Count() - 1];
+            }
+        }
+
+        public void StartServer(SocketIOServer socket)
+        {
+            socket.Start();
         }
 
         public void SetServerRules(Map map)
@@ -36,18 +53,18 @@ namespace SorcerIo.Services
             map.Socket.OnConnection((SocketIOSocket socket) => HandleSocketConnection(socket));
         }
 
-        public void DestroyMap(Player owner, ushort port)
+        public void DestroyMap(Player owner)
         {
-            Map? map = FindMap(owner, port);
+            Map? map = FindMap(owner);
             if (map != null)
             {
-                map.Socket.Stop();
-                _currentMaps.Remove(map);
+                _runningMaps.Remove(map);
             }
             else
             {
                 throw new Exception("Map does not exist!");
             }
+            SocketGarbageCollector();
         }
 
         public void HandleSocketConnection(SocketIOSocket socket)
@@ -55,10 +72,35 @@ namespace SorcerIo.Services
             Console.WriteLine($"Client connected: {socket.ToString()}");
         }
 
-        public Map? FindMap(Player owner, ushort port)
+        public ushort HandlePortGeneration()
         {
-            // consider only owner
-            return _currentMaps.Find(cmap => cmap.Owner == owner);
+            SocketIOServer lastAddedSocket = _runningSockets[_runningSockets.Count() - 1];
+            // eatch port can have 50 clients
+            if (lastAddedSocket.ClientsCounts > 50)
+            {
+                return Convert.ToUInt16(START_PORT + _runningSockets.Count()); // go to next port
+            }
+            else
+            {
+                return Convert.ToUInt16(START_PORT + (_runningSockets.Count() - 1)); // stay in the current port (START_PORT + socketsCount - 1)
+            }
+        }
+
+        public void SocketGarbageCollector()
+        {
+            foreach (SocketIOServer socket in _runningSockets)
+            {
+                if (socket.ClientsCounts == 0)
+                {
+                    socket.Stop();
+                    _runningSockets.Remove(socket);
+                }
+            }
+        }
+
+        public Map? FindMap(Player owner)
+        {
+            return _runningMaps.Find(cmap => cmap.Owner == owner);
         }
     }
 }
